5758b487c05ed78bf79e9c1f2720dc95bdaf0db0
Will Smith <43626141+WifiGuyWill@users.noreply.github.com>
2021-08-29 20:22:30 -0700

    Update main.js

--- API/static/assets/js/main.js
+++ API/static/assets/js/main.js
@@ -2,7 +2,6 @@
 Central Automation v0.1
 */
 
-#var api_url = "https://api.wifidownunder.com"
 var api_url = "http://10.10.10.13"
 var forcedTokenRefresh = true;
 var $SCRIPT_ROOT = '{{ request.script_root|tojson|safe }}';
